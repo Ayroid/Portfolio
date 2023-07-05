c9589202f8434634378d4e917bf58814f274fc02
Ayroid <ayushsk0000@gmail.com>
2023-07-05 07:27:05 +0530

    nav fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,33 @@
 function toggleNavbar() {
-    const navbar = document.getElementById('navbar-menu-list');
-    if (!navbar.classList.contains('move-down')) {
-        navbar.classList.add('move-down');
-        navbar.classList.remove('move-up');
-        navbar.style.display = 'flex';
-    } else if (!navbar.classList.contains('move-up')) {
-        navbar.classList.add('move-up');
-        navbar.classList.remove('move-down');
-        setTimeout(() => {
-            navbar.style.display = 'none';
-        }, 300);
-    }
+    if (window.innerWidth < 800) {
+        const navbar = document.getElementById('navbar-menu-list');
+        if (!navbar.classList.contains('move-down')) {
+            navbar.classList.add('move-down');
+            navbar.classList.remove('move-up');
+            navbar.style.display = 'flex';
+        } else if (!navbar.classList.contains('move-up')) {
+            navbar.classList.add('move-up');
+            navbar.classList.remove('move-down');
+            setTimeout(() => {
+                navbar.style.display = 'none';
+            }, 300);
+        }
 
-    const hamburger = document.getElementById('hamburger');
-    if (!hamburger.classList.contains('rotate-clock')) {
-        hamburger.classList.add('rotate-clock');
-        hamburger.classList.remove('rotate-anti-clock');
-        setTimeout(() => {
-            hamburger.style.rotate = '90deg';
-        }, 300);
-    }
-    else if (!hamburger.classList.contains('rotate-anti-clock')) {
-        hamburger.classList.add('rotate-anti-clock');
-        hamburger.classList.remove('rotate-clock');
-        setTimeout(() => {
-            hamburger.style.rotate = '0deg';
-        }, 300);
+        const hamburger = document.getElementById('hamburger');
+        if (!hamburger.classList.contains('rotate-clock')) {
+            hamburger.classList.add('rotate-clock');
+            hamburger.classList.remove('rotate-anti-clock');
+            setTimeout(() => {
+                hamburger.style.rotate = '90deg';
+            }, 300);
+        }
+        else if (!hamburger.classList.contains('rotate-anti-clock')) {
+            hamburger.classList.add('rotate-anti-clock');
+            hamburger.classList.remove('rotate-clock');
+            setTimeout(() => {
+                hamburger.style.rotate = '0deg';
+            }, 300);
+        }
     }
 }
 
